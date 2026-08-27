4ff5b1da25d96857531c341b5e266112a5582adf
Georg <55485342+gevorg14@users.noreply.github.com>
2026-08-27 05:21:46 +0000

    Pending changes exported from your codespace

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Online shop</title>
+</head>
+
+<body>
+    <div class="Navigation-bar">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <search>
+            <form action="/search" method="get">
+                <input type="search" name="q" placeholder="Search">
+                <button type="submit">Search</button>
+            </form>
+        </search>
+        <a href="#">Contact</a>
+    </div>
+
+</body>
+
 <?php
 
-echo('123');
+echo "123";
 
 ?>
